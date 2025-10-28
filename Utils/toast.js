d3b742c0e921f884e024afc26da06ab7a0ae3e5e
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-10-28 01:35:58 +0100

    .

--- Utils/toast.js
+++ Utils/toast.js
@@ -1,9 +1,9 @@
-export function showToast(message, type='success'){
-const root = document.createElement('div')
-root.className = `toast ${type}`
-root.textContent = message
-root.setAttribute('role','status')
-document.body.appendChild(root)
-setTimeout(()=> root.classList.add('show'), 10)
-setTimeout(()=> root.remove(), 3000)
+export function showToast(message, type = "success") {
+  const root = document.createElement("div");
+  root.className = `toast ${type}`;
+  root.textContent = message;
+  root.setAttribute("role", "status");
+  document.body.appendChild(root);
+  setTimeout(() => root.classList.add("show"), 10);
+  setTimeout(() => root.remove(), 3000);
 }
